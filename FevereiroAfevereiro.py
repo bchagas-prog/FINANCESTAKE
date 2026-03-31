00c2dacd5bc9516f550a876b0585d4e26ca24658
bchagas-prog <b.chagas@easygo.io>
2026-03-31 12:02:54 -0300

    Update FevereiroAfevereiro.py

--- FevereiroAfevereiro.py
+++ FevereiroAfevereiro.py
@@ -9,7 +9,7 @@ import datetime
 import time
 st.title("Finance Stake")
 
-st.write("App rodando 🚀")
+st.write("STAKE.BET.BR 🚀")
 # ==============================================================================
 # 1. ARQUITETURA DE UI E DESIGN EXECUTIVO (C-LEVEL DASHBOARD)
 # ==============================================================================
